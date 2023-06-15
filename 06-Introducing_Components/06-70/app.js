dae51ae2e640a5c1f89477e227a7b025d6c06eb4
Erdna's Kram <72553603+erdnaskram@users.noreply.github.com>
2023-06-15 16:00:37 +0200

    06-72

--- 06-Introducing_Components/06-70/app.js
+++ 06-Introducing_Components/06-70/app.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
+      detailsAreVisible: false,
       friends: [
         {
           id: "john",
@@ -27,33 +28,6 @@ const app = Vue.createApp({
           email: "susan.lee@outlook.com",
         },
       ],
-    };
-  },
-  methods: {},
-});
-
-app.component("friend-contact", {
-  template: `
-    <li :key="friend.id">
-        <h2>{{ friend.name }}</h2>
-        <button @click="toggleDetails">
-            {{ detailsAreVisible ? 'Hide' : 'Show'}} Details
-        </button>
-        <ul v-if="detailsAreVisible">
-            <li><strong>Phone:</strong> {{ friend.phone }}</li>
-            <li><strong>Email:</strong> {{ friend.email }}</li>
-        </ul>
-    </li>
-    `,
-  data() {
-    return {
-      detailsAreVisible: false,
-      friend: {
-        id: "susan",
-        name: "Susan Lee",
-        phone: "+49 162 45678901",
-        email: "susan.lee@outlook.com",
-      },
     };
   },
   methods: {
