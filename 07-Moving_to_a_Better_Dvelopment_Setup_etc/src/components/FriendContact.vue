988fc74c1a1ef06d2feddff34f3e479219c16c9b
Erdna's Kram <72553603+erdnaskram@users.noreply.github.com>
2023-07-10 13:55:04 +0200

    Vue Project style 7.75 -7.90

---
+++ 07-Moving_to_a_Better_Dvelopment_Setup_etc/src/components/FriendContact.vue
@@ -0,0 +1,36 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "susan",
+        name: "Susan Lee",
+        phone: "+49 162 45678901",
+        email: "susan.lee@outlook.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
