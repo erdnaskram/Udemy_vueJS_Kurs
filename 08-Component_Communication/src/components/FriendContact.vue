7820d20f9c51c2569e5371039d536ad9b7376251
Erdna's Kram <72553603+erdnaskram@users.noreply.github.com>
2023-07-26 17:56:59 +0200

    Kap8 - 97

---
+++ 08-Component_Communication/src/components/FriendContact.vue
@@ -0,0 +1,61 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavorite === true ? "(Favorite)" : "" }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ phone }}</li>
+      <li><strong>Email:</strong> {{ email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  //props: ["name", "phone", "email", "isFavorite"],
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+      validator: function (value) {
+        return typeof value === "boolean";
+      },
+    },
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      this.$emit("toggle-favorite", this.id);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
